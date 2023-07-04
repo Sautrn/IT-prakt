ad34cc05bf2bbd535033d4242e069851b32fb2e7
Saturn <129054991+Strishenets@users.noreply.github.com>
2023-07-04 09:14:53 +0300

    Add files via upload

---
+++ prak_1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t1, t2, t3;
+    float rez;               //Оголошення змінних
+
+    printf("Enter three values:\n");
+    scanf("%d %d %d", &t1, &t2, &t3);      //Введення даних
+
+    rez=(t1+t2+t3)/3;
+    rez=rez/3;   //Обчислення результату
+    printf("Time required to eat the cake: %0.2f", rez);    //Виведення результату
+
+    return 0;
+}
